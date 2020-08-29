47cdf6d6aa099a947d42dd77ed64e3be635429e0
VedPant <69761235+VedPant@users.noreply.github.com>
2020-08-28 22:53:50 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var bullet, wall, speed, weight, thichness;
+
+function setup() {
+  createCanvas(1600,400);
+
+  speed=random(223,321);
+  weight=random(30,52);
+  thickness=random(22,83);
+
+  bullet=createSprite(50,200,50,50);
+  bullet.velocityX=speed;
+
+  wall=createSprite(1200,200,thickness,height/2);
+  wall.shapeColor="Grey";
+}
+
+function draw() {
+  background(255,255,255);  
+
+  if(hasCollided(bullet, wall)){
+    bullet.velocityX=0;
+    var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
+    if(damage>10){
+      bullet.shapeColor=color(225,0,0);
+    }
+    if(damage<10){
+      bullet.shapeColor=color(0,255,0);
+    }
+  }
+
+  drawSprites();
+}
+
+function hasCollided(bullet, wall){
+  bulletRightEdge=bullet.x+bullet.width;
+  wallLeftEdge=wall.x;
+
+  if(bulletRightEdge>=wallLeftEdge){
+    return true
+  }
+  return false;
+}
